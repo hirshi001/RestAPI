85a8f27f6d5aea556c460b14a07cfefbe81ba6d2
Hrishikesh Ingle <ketchhrishikesh.ingleup@gmail.com>
2024-12-02 00:14:59 -0800

    attempting to add unit test

--- gwtrestapi/src/test/java/com/hirshi001/gwtrestapi/test/GWTRestTest.java
+++ gwtrestapi/src/test/java/com/hirshi001/gwtrestapi/test/GWTRestTest.java
@@ -1,14 +1,11 @@
 package com.hirshi001.gwtrestapi.test;
 
+import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.hirshi001.gwtrestapi.GWTRestFutureFactory;
 import com.hirshi001.restapi.*;
 
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicLong;
 
 
 public class GWTRestTest extends GWTTestCase {
@@ -20,182 +17,79 @@ public class GWTRestTest extends GWTTestCase {
         RestAPI.setFactory(new GWTRestFutureFactory());
     }
 
-
     public void testBasicMethods() throws InterruptedException {
 
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-        AtomicBoolean flag2 = new AtomicBoolean(false);
-        AtomicBoolean flag3 = new AtomicBoolean(false);
-        AtomicBoolean flag4 = new AtomicBoolean(false);
+        boolean[] flags = new boolean[5];
 
-        AtomicLong start = new AtomicLong();
+        Long[] start = new Long[1];
         RestFuture<?, String> future = RestAPI.create(()->"Hi");
         RestFuture<Integer, Integer> f2 = future
                 .then((s)->{
-                    flag1.set(true);
+                    flags[0] = true;
                     assert s.equals("Hi");
                 })
                 .then(RestAPI.create(()->{
-                    flag4.set(true);
+                    flags[1] = true;
                     return null;
                 }))
                 .map(s->{
-                    flag2.set(true);
+                    flags[2] = true;
                     return s.hashCode();
                 })
                 .then((i)->{
-                    start.set(System.currentTimeMillis());
+                    flags[3] = true;
+                    start[0] = System.currentTimeMillis();
                 })
                 .pauseFor(1, TimeUnit.SECONDS)
                 .then((i)->{
-                    long diff = System.currentTimeMillis() - start.get();
+                    long diff = System.currentTimeMillis() - start[0];
                     if(diff > 980 && diff < 1020){
-                        flag3.set(true);
+                        flags[4] = true;
                     }
                 }).perform();
 
 
-        Thread.sleep(1200);
+        // run this code 1000ms later
+        Scheduler.get().scheduleFixedDelay(() -> {
+            assertTrue(flags[0]);
+            assertTrue(flags[1]);
+            assertTrue(flags[2]);
+            assertTrue(flags[3]);
+            assertTrue(flags[4]);
 
-        assertTrue(flag1.get());
-        assertTrue(flag2.get());
-        assertTrue(flag3.get());
-        assertTrue(flag4.get());
+            assertTrue(f2.isDone());
+            assertTrue(f2.isSuccess());
+            assertNull(f2.cause());
+            assertFalse(f2.isCancelled());
+            assertFalse(f2.isFailure());
 
-        assertTrue(f2.isDone());
-        assertTrue(f2.isSuccess());
-        assertNull(f2.cause());
-        assertFalse(f2.isCancelled());
-        assertFalse(f2.isFailure());
+            return false;
+        }, 1200);
+
 
     }
 
     public void testBothMethods() throws InterruptedException {
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-        AtomicBoolean flag2 = new AtomicBoolean(false);
-        AtomicBoolean flag3 = new AtomicBoolean(false);
-        AtomicBoolean flag4 = new AtomicBoolean(false);
+        boolean[] flags = new boolean[4];
 
         RestFuture<String, String> future = RestAPI.create();
         future
-                .then(s -> flag1.set(true))
-                .thenBoth(RestAPI.<String>create().then(s -> flag2.set(true)),
-                        RestAPI.<String>create().then(s -> flag3.set(true)))
-                .then(s -> flag4.set(true));
+                .then(s -> flags[0] = true)
+                .thenBoth(RestAPI.<String>create().then(s -> flags[1] = true),
+                        RestAPI.<String>create().then(s -> flags[2] = true))
+                .then(s -> flags[3] = true);
 
         future.perform("Test");
-        Thread.sleep(100);
-
-        assertTrue(flag1.get());
-        assertTrue(flag2.get());
-        assertTrue(flag3.get());
-        assertTrue(flag4.get());
-    }
-
-    public void testAsyncMethods() throws InterruptedException {
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-        AtomicBoolean flag2 = new AtomicBoolean(false);
-        AtomicBoolean flag3 = new AtomicBoolean(false);
-        AtomicBoolean flag4 = new AtomicBoolean(false);
-        AtomicBoolean flag5 = new AtomicBoolean(false);
-        AtomicBoolean flag6 = new AtomicBoolean(false);
-
-        Thread mainThread = Thread.currentThread();
-
-        RestFuture<String, String> future = RestAPI.create();
-
-        future
-                .then(s -> flag1.set(true))
-                .thenAsync(s -> {
-                    flag2.set(true);
-                    assertNotSame(mainThread, Thread.currentThread());
-                })
-                .then(s -> flag3.set(true))
-                .map(s->5)
-                .thenBothAsync(
-                        RestAPI.<Integer>create().then(s -> {
-                            flag4.set(true);
-                            assertNotSame(mainThread, Thread.currentThread());
-                        }),
-                        RestAPI.<Integer>create().then(s -> {
-                            flag5.set(true);
-                            assertNotSame(mainThread, Thread.currentThread());
-                        }))
-                .then(s -> flag6.set(true));
-
-        future.perform("Test");
-        Thread.sleep(100);
-
-        assertTrue(flag1.get());
-        assertTrue(flag2.get());
-        assertTrue(flag3.get());
-        assertTrue(flag4.get());
-        assertTrue(flag5.get());
-        assertTrue(flag6.get());
-
-    }
-
-    public void testGet() throws ExecutionException, InterruptedException {
-        String message = "Test";
-
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-
-        RestFuture<?, String> future = RestAPI.create((f, i)->
-                f.getScheduledExec().run( ()-> f.taskFinished(message),
-                        100,
-                        TimeUnit.MILLISECONDS));
-
-        future = future.then(s -> flag1.set(true));
-        future.perform();
 
 
-        assertEquals(message, future.get());
-        assertTrue(flag1.get());
-    }
+        Scheduler.get().scheduleFixedDelay(() -> {
+            assertTrue(flags[0]);
+            assertTrue(flags[1]);
+            assertTrue(flags[2]);
+            assertTrue(flags[3]);
+            return false;
+        }, 100);
 
-    public void testDelaySuccess() throws ExecutionException, InterruptedException, TimeoutException {
-        String message = "Test";
-
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-
-        RestFuture<String, String> future = RestAPI.<String>create((f, i)->{
-            f.getScheduledExec().run( ()-> f.taskFinished(message), 100, TimeUnit.MILLISECONDS);
-        }).then(s -> flag1.set(true));
-        future.performAsync();
-
-        assertEquals(message, future.get(200, TimeUnit.MILLISECONDS));
-        assertTrue(flag1.get());
-    }
-
-    public void testDelayFail() {
-        String message = "Test";
-
-        AtomicBoolean flag1 = new AtomicBoolean(false);
-
-        RestFuture<String, String> future = RestAPI.<String>create((f, i)->{
-            f.getScheduledExec().run( ()-> f.taskFinished(message), 100, TimeUnit.MILLISECONDS);
-        }).then(s -> flag1.set(true)).performAsync();
-
-        assertThrows(TimeoutException.class, () -> future.get(50, TimeUnit.MILLISECONDS));
-        assertFalse(flag1.get());
-    }
-
-
-    public void testListenerError(){
-        RestFuture<?, String> future = RestAPI.create();
-        RestFutureListener<?, String> listener = new RestFutureListener<Object, String>() {
-            @Override
-            public void onComplete(RestFuture<Object, String> future) {
-
-            }
-        };
-
-        assertThrows(NullPointerException.class, ()->future.addListener(null));
-        assertDoesNotThrow(()->future.addListener(listener));
-
-        assertThrows(NullPointerException.class, ()->future.addListener(null, listener));
-        assertThrows(NullPointerException.class, ()->future.addListener(future.getScheduledExec(), null));
-        assertDoesNotThrow(()->future.addListener(future.getScheduledExec(), listener));
     }
 
     private void assertDoesNotThrow(ThrowableRunnable runnable) {
@@ -212,7 +106,7 @@ public class GWTRestTest extends GWTTestCase {
             o.run();
             throw new AssertionError("Expected exception of type: " + timeoutExceptionClass.getName());
         } catch (Throwable t) {
-            
+
             throw new AssertionError("Expected exception of type: " + timeoutExceptionClass.getName() + " but got: ", t);
         }
     }
@@ -225,6 +119,6 @@ public class GWTRestTest extends GWTTestCase {
 
     @Override
     public String getModuleName() {
-        return "com.hirshi001.gwtrestapi.GWTRestAPI";
+        return "com.hirshi001.gwtrestapi.test.Test";
     }
 }
